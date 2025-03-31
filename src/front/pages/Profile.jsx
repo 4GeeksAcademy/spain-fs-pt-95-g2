079d3ae0532ad764df58daa8ef45a9a0fd104a86
Eric <ericmulerofernandez@gmail.com>
2025-03-31 18:13:17 +0000

    arreglado los errores, creo

--- src/front/pages/Profile.jsx
+++ src/front/pages/Profile.jsx
@@ -3,14 +3,19 @@ import React, { useEffect, useState } from "react";
 const Profile = () => {
   const [user, setUser] = useState(null);
 
+  const PERMISSIONS = {
+    1: "Visualizer",
+    2: "Admin"
+  };
+
   useEffect(() => {
     setUser({
-      username: "ericMulero",
-      email: "ericemail@email.com",
-      created_date: "31/03/2025",
+      username: "",
+      email: "",
+      created_date: "new_date",
       inventories: [
-        { name: "la mate por un petisuisse", permissions: 1 },
-        { name: "tres pistola pa un manco", permissions: 2 },
+        { name: "tienda 1", permissions: 1 },
+        { name: "tienda 2", permissions: 2 },
       ],
     });
   }, []);
@@ -19,18 +24,18 @@ const Profile = () => {
 
   return (
     <div className="profile-container">
-      <h1 className="title">User Profile</h1>
+      <h1 className="titulo">User Profile</h1>
       <div className="card">
         <p><strong>User Name:</strong> {user.username}</p>
         <p><strong>Email:</strong> {user.email}</p>
-        <p><strong>Member Since:</strong> {user.created_date}</p>
+        <p><strong>Member since:</strong> {user.created_date}</p>
       </div>
       <div className="inventories">
-        <h2 className="stores">Assigned Inventories</h2>
-        <ul className="inventories-list">
+        <h2 className="stores">Assigned inventories</h2>
+        <ul className="inventorieslist">
           {user.inventories.map((inv, idx) => (
             <li key={idx}>
-              {inv.name} - Permission: {inv.permissions === 2 ? "Admin" : "Visualizer"}
+              {inv.name} - Permission: {PERMISSIONS[inv.permissions]}
             </li>
           ))}
         </ul>
