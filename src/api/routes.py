3a59c0d903b9a0d91590d0f3193b99f0e8d02328
Eric <ericmulerofernandez@gmail.com>
2025-03-31 18:58:11 +0000

    arreglado las eliminaciones2

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,3 +1,6 @@
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
 from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_cors import CORS
