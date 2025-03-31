5c8ecae344b9957110a38452e065ad4193550bcf
Eric <ericmulerofernandez@gmail.com>
2025-03-31 16:52:08 +0000

    creado el profile en pages

--- src/front/pages/Profile.jsx
+++ src/front/pages/Profile.jsx
@@ -1,45 +1,42 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Profile = () => {
-    const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        setUser({
-            username: "ericMulero",
-            email: "ericemail@email.com",
-            created_date:"31/03/2025",
-            inventories: [
-                { name:"la mate por un petisuisse",permisions:1},
-                { name:"tres pistola pa un manco",permisions:2},
+  useEffect(() => {
+    setUser({
+      username: "ericMulero",
+      email: "ericemail@email.com",
+      created_date: "31/03/2025",
+      inventories: [
+        { name: "la mate por un petisuisse", permissions: 1 },
+        { name: "tres pistola pa un manco", permissions: 2 },
+      ],
+    });
+  }, []);
 
-            ],
-        });
-    }, []);
+  if (!user) return <p className="text-center">Loading profile...</p>;
 
-    if (!user) return < p ClassName="text-center">loading profile...</p>;
-}
-
-    return (
-        <div className="profile-container">
-            <h1 className="tittle">User Profile</h1>
-            <div className="card">
-                <p><strong>User Name</strong> {user.username}</p>
-                <p><strong>Email</strong>{user.email}</p>
-                <p><strong>Member Since:</strong>{user.created_date}</p>
-        </div>
-        <div className="inventories">
-            <h2 className="stores">Assigned inventories</h2>
-            <ul className="inventories list">
-            {user.inventories.map((inv,idx) => (
-                <li key={idx}>
-                {inv.name} - Permission: {inv.permissions === 2 ? "Admin" : "Visualizer"}
-                </li>
-            ))}
-
-            </ul>
-        </div>
-        </div>
-    );
-    
+  return (
+    <div className="profile-container">
+      <h1 className="title">User Profile</h1>
+      <div className="card">
+        <p><strong>User Name:</strong> {user.username}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+        <p><strong>Member Since:</strong> {user.created_date}</p>
+      </div>
+      <div className="inventories">
+        <h2 className="stores">Assigned Inventories</h2>
+        <ul className="inventories-list">
+          {user.inventories.map((inv, idx) => (
+            <li key={idx}>
+              {inv.name} - Permission: {inv.permissions === 2 ? "Admin" : "Visualizer"}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
