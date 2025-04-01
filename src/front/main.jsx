be3780d5b6b89c24910dcc1ebd5cfa1f08484287
Eric <ericmulerofernandez@gmail.com>
2025-04-01 08:10:24 +0000

    solucionando minucias

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
-import { StoreProvider } from './hooks/useGlobalReducer';
-import { BackendURL } from './components/BackendURL';
+import ReactDOM from 'react-dom/client'
+import './index.css'// Global styles for your application
+import { RouterProvider } from "react-router-dom"// Import RouterProvider to use the router
+import { router } from "./routes"// Import the router configuration
+import { StoreProvider } from './hooks/useGlobalReducer'// Import the StoreProvider for global state management
+import { BackendURL } from './components/BackendURL'// Componente para establecer la URL del backend si no está definida
 
 const Main = () => {
     if (!import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_BACKEND_URL == "") {
