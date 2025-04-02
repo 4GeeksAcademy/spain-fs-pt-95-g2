5d3416593d7e842ecf7a3f7354d57f6b6cfa388c
dualss <dariongduals@gmail.com>
2025-04-02 14:51:03 +0000

    Pequeña mejora para el componente

--- src/front/components/DetailedCard.jsx
+++ src/front/components/DetailedCard.jsx
@@ -1,11 +1,11 @@
-export const DetailedCard = ({ imageProduct, name, price, quantity, category }) => {
+export const DetailedCard = ({ Product }) => {
     <div className="card">
-        <img src={imageProduct} className="card-img-top"></img>
+        <img src={Product.imageProduct} className="card-img-top"></img>
         <div className="card-body">
-            <h5>{name}</h5>
-            <p>{category}</p>
-            <p>{price}</p>
-            <p>{quantity}</p>
+            <h5>{Product.name}</h5>
+            <p>{Product.category}</p>
+            <p>{Product.price}</p>
+            <p>{Product.quantity}</p>
         </div>
     </div>
 }
