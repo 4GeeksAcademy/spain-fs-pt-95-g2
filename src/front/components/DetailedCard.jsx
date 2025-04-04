bc29f9ed127ba1f05b076e110f3c95cc0e5f372f
dualss <dariongduals@gmail.com>
2025-04-04 18:46:28 +0200

    Update DetailedCard.jsx

--- src/front/components/DetailedCard.jsx
+++ src/front/components/DetailedCard.jsx
@@ -1,6 +1,6 @@
 export const DetailedCard = ({ product }) => {
     <div className="card">
-        <img src={roduct.image_url} className="card-img-top"></img>
+        <img src={product.image_url} className="card-img-top"></img>
         <div className="card-body">
             <h5>{product.name}</h5>
             <p>{product.category}</p>
