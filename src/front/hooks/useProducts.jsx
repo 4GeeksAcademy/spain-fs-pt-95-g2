882c46c828e4bf5307eb4992865e47f3e4e8054e
dualss <dariongduals@gmail.com>
2025-04-14 19:00:04 +0000

    Traido archivo necesario para hacer pruebas

---
+++ src/front/hooks/useProducts.jsx
@@ -0,0 +1,131 @@
+import { useState, useEffect } from "react";
+
+const API_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc0NDY0OTIwNywianRpIjoiODQ4MGQ2NGQtZjdjZS00ZWM0LWE4YjItOGM5NTQ2ZTg3NjQwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjEiLCJuYmYiOjE3NDQ2NDkyMDcsImNzcmYiOiI0MzQ5OTU1My04NWU2LTQxZTktYmYwZC1kMzhiMjY5NDZlOGYiLCJleHAiOjE3NDQ3MzU2MDd9.6t1pBwolQz64qPGwhhV5ASYupD3tQ_57tOZEHkW5Cco";
+
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    setError("");
+    try {
+      const response = await fetch(`${API_URL}/api/products`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      if (!response.ok) throw new Error("No se pudieron cargar los productos.");
+      const dataProduct = await response.json();
+      setProducts(dataProduct);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchProductsId = async (id) => {
+    setLoading(true);
+    setError("");
+    try {
+      const response = await fetch(`${API_URL}/api/products/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      if (!response.ok) throw new Error("No se pudo cargar el producto.");
+      const dataProduct = await response.json();
+      return dataProduct;
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createProduct = async (newProductData) => {
+    try {
+      const response = await fetch(`${API_URL}/api/products`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(newProductData)
+      });
+
+      if (!response.ok) throw new Error("Error al crear producto");
+      const newProduct = await response.json();
+      setProducts(prev => [...prev, newProduct]);
+      return newProduct;
+    } catch (error) {
+      setError(error.message);
+      throw error;
+    }
+  };
+
+  const updateProduct = async (productId, updatedProductData) => {
+    try {
+      console.log("entra")
+      const response = await fetch(`${API_URL}/api/products/${productId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(updatedProductData)
+      });
+      console.log(response)
+      if (!response.ok) throw new Error("Error al actualizar el producto");
+      const updateProduct = await response.json();
+      setProducts(prev => prev.map(product => product.id_product === productId ? updateProduct : product));
+      return updateProduct;
+    } catch (error) {
+      setError(error.message);
+      throw error;
+    }
+  };
+
+  const deleteProduct = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/api/products/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) throw new Error("Error al eliminar el producto");
+
+      setProducts((prev) => prev.filter((p) => p.id !== id));
+    } catch (error) {
+      setError(error.message);
+      throw error;
+    }
+  };
+
+
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+
+  return {
+    products,
+    error,
+    loading,
+    fetchProducts,
+    fetchProductsId,
+    createProduct,
+    updateProduct,
+    deleteProduct
+  };
+};
