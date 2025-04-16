491987895323a79ca687b3a62e8d78a942c4cc2c
Antonio Rengifo Daza <antoren9708@gmail.com>
2025-04-16 17:04:31 +0000

    Se integro todo en una sola pagina

--- src/front/pages/ProductList.jsx
+++ src/front/pages/ProductList.jsx
@@ -1,14 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useProducts } from "../hooks/useProducts";
-import { useNavigate } from "react-router-dom";
+import ProductEditForm from "../components/ProductEditForm";
+import ProductForm from "../components/ProductForm";
 
 const ProductList = () => {
-  const { products, fetchProducts, deleteProduct, loading, error } = useProducts();
-  const navigate = useNavigate();
-
-  const handleFormEdit = (product) => {
-    navigate(`/products/edit/${product.id_product}`);
-  };
+  const { products, fetchProducts, deleteProduct, loading } = useProducts();
+  const [productEdit, setProductEdit] = useState(null);
+  const [showCreateForm, setShowCreateForm] = useState(false);
 
   useEffect(() => {
     fetchProducts();
@@ -28,44 +26,71 @@ const ProductList = () => {
 
   return (
     <div className="container mt-4">
-      <h2 className="mb-4">Gestión de Productos</h2>
-      {loading ? (
-        <p>Cargando productos...</p>
+      {productEdit ? (
+        <ProductEditForm
+          product={productEdit}
+          onUpdate={() => {
+            fetchProducts();
+            setProductEdit(null)
+          }}
+          onCancel={() => {
+            fetchProducts();
+            setProductEdit(null)
+          }
+          }
+        />
+      ) : showCreateForm ? (
+        <ProductForm
+          onSuccess={() => {
+            fetchProducts();
+            setShowCreateForm(false)
+          }}
+          onCancel={() => {
+            setShowCreateForm(false)
+          }}
+        />
       ) : (
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="bg-dark text-white">ID</th>
-              <th className="bg-dark text-white">Name</th>
-              <th className="bg-dark text-white">Price</th>
-              <th className="bg-dark text-white">Quantity</th>
-              <th className="bg-dark text-white">Accion</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product.id_product}>
-                <td>{product.id_product}</td>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
-                <td>{product.quantity}</td>
-                <td>
-                  <button className="mx-2" onClick={() => handleFormEdit(product)}>Edit</button>
-                  <button onClick={() => handleFormDelete(product.id_product)}>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <>
+          <h2 className="mb-4">Gestión de Productos</h2>
+          {loading ? (
+            <p>Cargando productos...</p>
+          ) : (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th className="bg-dark text-white">ID</th>
+                  <th className="bg-dark text-white">Name</th>
+                  <th className="bg-dark text-white">Price</th>
+                  <th className="bg-dark text-white">Quantity</th>
+                  <th className="bg-dark text-white">Accion</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((product) => (
+                  <tr key={product.id_product}>
+                    <td>{product.id_product}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.quantity}</td>
+                    <td>
+                      <button className="mx-2" onClick={() => setProductEdit(product)}>Edit</button>
+                      <button onClick={() => handleFormDelete(product.id_product)}>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+          <div className="d-flex justify-content-center">
+            <button
+              className="btn btn-primary mb-3"
+              onClick={() => setShowCreateForm(true)}
+            >
+              Crear Producto
+            </button>
+          </div>
+        </>
       )}
-      <div className="d-flex justify-content-center">
-        <button
-          className="btn btn-primary mb-3"
-          onClick={() => navigate("/products/create")}
-        >
-          Crear Producto
-        </button>
-      </div>
     </div>
   );
 };
